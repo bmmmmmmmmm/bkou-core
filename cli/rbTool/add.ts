5c68c70d1e9dcdb4d302212ca679fef5ab90affc
Benming Kou <koubm@300624.cn>
2024-05-16 12:17:08 +0000

    CHG: \n => EOL

--- cli/rbTool/add.ts
+++ cli/rbTool/add.ts
@@ -1,8 +1,9 @@
 import { existsSync, appendFileSync, writeFileSync } from 'node:fs'
+import { EOL } from 'node:os'
 
 const _addRB = async (filePath, content: string) => {
   const date = new Date();
-  const text = `${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}::${content}\r\n`;  
+  const text = `${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}::${content}${EOL}`;
 
   if (existsSync(filePath)) {
     appendFileSync(filePath, text);
--- cli/rbTool/list.ts
+++ cli/rbTool/list.ts
@@ -1,11 +1,13 @@
 import { existsSync, readFileSync } from 'node:fs'
-import { countWords } from '../countWords';
+import { EOL } from 'node:os'
+
+import { countWords } from '../countWords'
 
 const _listRB = (filePath) => {
   if (existsSync(filePath)) {
     const content = readFileSync(filePath, 'utf8');
     const lines = content.split('\n');
-    const result = lines.map(line => line.split('::')[1]).join('\n');
+    const result = lines.map(line => line.split('::')[1]).join(EOL);
     const count = countWords(result)
     console.log(`${result}\n============\nTotal words: ${count}`)
   } else {
